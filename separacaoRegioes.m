f3942ee4f3b95c09ac69d666dc4d53ba6edf1f67
phelyppe-alex <phelyppesilva@gmail.com>
2022-09-27 23:29:57 -0300

    pratica 5

--- separacaoRegioes.m
+++ separacaoRegioes.m
@@ -2,33 +2,32 @@ clear all
 close all
 pkg load image
 
-im = imread('C:\Users\Aluno\Pictures\PDI\objetos1.jpg');
+im = imread('/home/phelyppe-alex/Imagens/PDI/objetos1.jpg');
 figure('Name', 'Imagem Original')
 imshow(im)
 
 imBW = logical(im); % Transformando a imagem em binária
-matrizRegioes = zeros(im); % criando uma matriz para salvar as regiões
+matrizRegioes = zeros(size(im,1), size(im,2)); % criando uma matriz para salvar as regiões
 
 contadorRegioes = 2;
 for(i=2:size(imBW, 1)-1)
   for(j=2:size(imBW, 2)-1)
-    % Inicializando o primeiro índice
-    if(i==2 && j==2)
+
+    if(imBW(i,j) == 0)
       matrizRegioes(i,j) = 1;
-    endif
-    % Percorrendo primeira linha
-    if(i==2 && j>2)
-      if(imBW(i,j-1) == imBW(i,j))
-        matrizRegioes(i,j) = imBW(i,j-1);
+    else
+      if((imBW(i,j-1) == 1) || (imBW(i-1,j) == 1))
+        if(imBW(i,j-1) == 1)
+          matrizRegioes(i,j) = matrizRegioes(i,j-1);
+        endif
+        if(imBW(i-1,j) == 1)
+          matrizRegioes(i,j) = matrizRegioes(i-1,j);
+        endif
       else
         matrizRegioes(i,j) = contadorRegioes;
-        contadorRegioes++;
+        contadorRegioes = contadorRegioes + 1;
       endif
-    % Percorrendo o restante da imagem
-    else
-      if()
-
-      else
     endif
+
   endfor
 endfor
