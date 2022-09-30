4ae7b7c6f4f81ff8d82464678e8b1bfc68b4a339
phelyppe-alex <phelyppesilva@gmail.com>
2022-09-30 10:43:30 -0300

    Cont. pratica 5

--- separacaoRegioes.m
+++ separacaoRegioes.m
@@ -3,31 +3,38 @@ close all
 pkg load image
 
 im = imread('/home/phelyppe-alex/Imagens/PDI/objetos1.jpg');
-figure('Name', 'Imagem Original')
-imshow(im)
+% figure('Name', 'Imagem Original')
+% imshow(im)
 
-imBW = logical(im); % Transformando a imagem em binária
+% Transformando a imagem em binária por atribuição direta condicional
+imBW = im;
+imBW(imBW<=128) = 0;
+imBW(imBW>128) = 1;
+imBW = logical(imBW);
+
+figure('Name', 'Imagem Binária')
+imshow(imBW)
+
 matrizRegioes = zeros(size(im,1), size(im,2)); % criando uma matriz para salvar as regiões
 
-contadorRegioes = 2;
+contadorRegioes = 1;
 for(i=2:size(imBW, 1)-1)
-  for(j=2:size(imBW, 2)-1)
-
-    if(imBW(i,j) == 0)
-      matrizRegioes(i,j) = 1;
-    else
-      if((imBW(i,j-1) == 1) || (imBW(i-1,j) == 1))
-        if(imBW(i,j-1) == 1)
-          matrizRegioes(i,j) = matrizRegioes(i,j-1);
+    for(j=2:size(imBW, 2)-1)
+        if(imBW(i,j) == 1)
+            if(imBW(i-1,j) + imBW(i,j-1) == 0)
+                matrizRegioes(i,j) = contadorRegioes;
+                contadorRegioes++;
+            endif
+            if(imBW(i-1,j) + imBW(i,j-1) >= 1)
+                if(imBW(i-1,j) == 1)
+                    matrizRegioes(i,j) = matrizRegioes(i-1,j);
+                else
+                    matrizRegioes(i,j) = matrizRegioes(i,j-1);
+                endif
+            endif
         endif
-        if(imBW(i-1,j) == 1)
-          matrizRegioes(i,j) = matrizRegioes(i-1,j);
-        endif
-      else
-        matrizRegioes(i,j) = contadorRegioes;
-        contadorRegioes = contadorRegioes + 1;
-      endif
-    endif
-
-  endfor
+    endfor
 endfor
+
+figure('Name', 'Separação com erros')
+imhist(matrizRegioes)
