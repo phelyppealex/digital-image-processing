b8b0fc358158492197d50a9b9e4335e57cc33e58
phelyppe-alex <phelyppesilva@gmail.com>
2022-10-04 18:26:45 -0300

    versão final da prática 5

--- separacaoRegioes.m
+++ separacaoRegioes.m
@@ -18,6 +18,7 @@ imshow(imBW)
 matrizRegioes = uint8(zeros(size(im,1), size(im,2))); % criando uma matriz para salvar as regiões
 
 contadorRegioes = 1;
+erros = [0 0];
 for(i=2:size(imBW,1)-1)
     for(j=2:size(imBW,2)-1)
         if(imBW(i,j) == 1)
@@ -32,6 +33,13 @@ for(i=2:size(imBW,1)-1)
                     matrizRegioes(i,j) = matrizRegioes(i,j-1);
                 endif
             endif
+            if((imBW(i-1,j) + imBW(i,j-1) == 2) && (matrizRegioes(i-1,j) != matrizRegioes(i,j-1)))
+                if(matrizRegioes(i-1,j) < matrizRegioes(i,j-1))
+                    erros = [erros;matrizRegioes(i-1,j) matrizRegioes(i,j-1)];
+                else
+                    erros = [erros;matrizRegioes(i,j-1) matrizRegioes(i-1,j)];
+                endif
+            endif    
         endif
     endfor
 endfor
@@ -39,8 +47,20 @@ endfor
 figure('Name', 'Separação com erros')
 imshow(matrizRegioes, [0, contadorRegioes])
 
-%for(i=2:size(imBW,1)-1)
-%    for(j=2:size(imBW,2)-1)
-%        if()
-%    endfor
-%endfor
+erros(1,1) = erros(2,1);
+erros(1,2) = erros(2,2);
+erros = unique(sort(erros), 'rows');
+
+for(k=1:size(erros,1))
+    for(i=1:size(matrizRegioes,1))
+        for(j=1:size(matrizRegioes,2))
+            if(matrizRegioes(i,j) == erros(k,1))
+                matrizRegioes(i,j) = erros(k,2);
+            endif
+        endfor
+    endfor
+endfor
+
+figure('Name', 'Imagem final')
+imshow(matrizRegioes, [0 erros(size(erros,1),2)])
+title(size(unique(matrizRegioes),1)-1)
