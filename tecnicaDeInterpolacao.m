4f9c215d8c099df3f285a533117eb33592090749
Phelyppe Álex <phelyppealex@pop-os.localdomain>
2022-10-17 17:46:06 -0300

    Interpolação

---
+++ tecnicaDeInterpolacao.m
@@ -0,0 +1,25 @@
+clear all
+close all
+pkg load image
+
+im = imread('/home/phelyppealex/Documentos/digital-image-processing/Lena.jpg');
+figure('Name','Imagem original: Lena.jpg')
+imshow(im)
+
+imCinza = rgb2gray(im);
+
+imPeq = zeros(size(imCinza,1)/2,size(imCinza,2)/2);
+
+contY = 1;
+contX = 1;
+for(i=1:2:size(imCinza,1)-1)
+    for(j=1:2:size(imCinza,2)-1)
+        imPeq(contX,contY) = imCinza(i,j);
+        contY += 1;
+    endfor
+    contY=1;
+    contX += 1;
+endfor
+
+figure('Name','Imagem reduzida')
+imshow(imPeq)
